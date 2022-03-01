f42f24fe7dae1b560e0240c945c040956cb770db
Michal Wakulinski <michal.wakulinski1@gmail.com>
2022-03-01 14:11:38 +0100

    delete coments

--- group.js
+++ group.js
@@ -2,9 +2,6 @@ const { v4: uuidv4 } = require("uuid");
 const Validator = require("./validator");
 const Contact = require("./contact");
 
-// Ma mieć: listę kontaktów oraz nazwę grupy oraz uuid
-// Ma umożliwiać: zmianę nazwy grupy, można dodać lub usunac kontakt z grupy, można sprawdzić czy kontakt istnieje w grupie
-
 class Group {
   constructor(name) {
     this.name = name;
@@ -24,8 +21,6 @@ class Group {
   addContact(contact) {
     Validator.throwIfNotProperInstance(contact, Contact);
 
-    //po idku
-    //pytanie lepiej wyrzucić error, czy pominąć akcję jak program znajdzie już istniejący kontakt
     if (!this.findContact(contact.id)) {
       this.contacts.push(contact);
     } else {
@@ -33,7 +28,6 @@ class Group {
     }
   }
 
-  //w sumie czemu filter lepszy?
   deleteContact(contactId) {
     Validator.throwIfNotString(contactId);
     this.contacts = this.contacts.filter((contact) => contact.id !== contactId);
@@ -45,15 +39,5 @@ class Group {
     );
   }
 }
-
-// const contact1 = new Contact("MIchal", "Wakulinski", "michal.wak@op.pl");
-// const contact2 = new Contact("Barbara", "Wakulinski", "michal.wak@op.pl");
-// const group = new Group("Barany");
-// group.addContact(contact1);
-// group.addContact(contact2);
-// group.deleteContact(contact1.id);
-// // group.deleteContact(contact1);
-// // group.deleteContact(contact2);
-// console.log(group);
 
 module.exports = Group;
